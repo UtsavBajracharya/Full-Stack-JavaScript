3df6830ab6de0c3ff5f12fa5d72a3e4608b95f20
Utsav <utsavbajra619@gmail.com>
2024-09-11 10:11:19 -0400

    simple node js application using express framework

--- Week2/ServerOne.js
+++ Week2/ServerOne.js
@@ -1,21 +1,22 @@
-express = require('express');
+express = require("express");
 app = express();
 
 app.get("/", (req, resp) => {
-
-    resp.sent("Welcome to Main Page!")
-
-})
+  resp.sent("Welcome to Main Page!");
+});
 
 app.get("/sales", (req, resp) => {
+  resp.sent("Welcome to Sales Department!");
+});
 
-    resp.sent("Welcome to Sales Department!")
+app.get("/sales/:name", (req, res) => {
 
-})
+  res.send(`We ran out of ${req.params.name}`);
+});
 
-
-
-
+app.listen(3000, '127.0.0.1');
 
 //In order to send a HTML file.
-Server.listen(3000,'127.0.0.1');
+// app.listen(3000, () => {
+//   console.log("Server is listening on por 3000");
+// });
